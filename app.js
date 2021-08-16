8cfd3432c76a3ad6535b173d97c054e002a20d1b
iamAzhar <mohdazhar6117@gmail.com>
2021-08-17 00:24:49 +0800

    store session data in database

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo');
+require('dotenv').config();
+const { DBURI } = process.env;
+
+//express app
 const app = express();
 
 //middleware
@@ -14,29 +20,47 @@ app.use(session({
     saveUninitialized: true,
     secret: 'this is my secret',
     cookie: {
-        maxAge: 20000
-    }
+        maxAge: 1000*60*60*5
+    },
+    store: MongoStore.create({ mongoUrl: process.env.DBURI })
 }));
 
+//db connection
+mongoose.connect(DBURI, { 
+    useUnifiedTopology:true,
+    useNewUrlParser:true
+},()=> {
+    app.listen(process.env.PORT || 3700, () => {
+        console.log('web started at port:' + 3700)
+    });
+})
 
-//PORT
-const PORT = 3700;
-app.listen(PORT, () => {
-    console.log('web started at port:' + PORT)
-});
 
 //Dummy data
 let users = [
     { id: 1, name: 'chris', password: 'i always beeen afraid' }
 ];
 
+//AUTHORIZATION Middleware
+const checkAuth = (req, res, next) => {
+    if (!req.session.user) {
+        req.session.err_msg = 'You need to be logged in to access this page';
+        res.redirect('/login');
+    } else {
+        next()
+    }
+}
+
 //ROUTES
 app.get('/', (req, res) => {
-    res.send(`${req.session.user?`
+    res.send(`${req.session.user ? `
+    <a href = "/mycookie">cookie</a>
     <a href = "/view">View</a>
-    `:`
+    <a href = "/logout">Log out</a>
+    `: `
+    <a href = "/mycookie">cookie</a>
     <a href = "/signup">Sign up</a>
-    <a href = "/signup">Log In</a>
+    <a href = "/login">Log In</a>
     `}`)
 })
 
@@ -54,13 +78,14 @@ app.get('/signup', (req, res) => {
 
 //login form
 app.get('/login', (req, res) => {
-    res.send(`<form action="/login" method="POST">
+    res.send(`${req.session.err_msg ? `<div style="color:red">${req.session.err_msg}</div>` : ''}
+    <form action="/login" method="POST">
     <h3>Login</h3>
     <label for="name">Name</label>
     <input type="text" name="name"> 
     <label for="name">Password</label>
     <input type="text" name="password">
-    <button type="submit">Sign up</button> 
+    <button type="submit">Log In</button> 
 </form>`);
 })
 
@@ -75,18 +100,41 @@ app.post('/signup', (req, res) => {
     users.push(user);
     res.redirect('/');
 })
+//login
+app.post('/login', (req, res) => {
+    const user = {
+        id: users.length + 1,
+        name: req.body.name,
+        password: req.body.password
+    }
+    req.session.user = user;
+    users.push(user);
+    res.redirect('/');
+})
 
 //view user
-app.get('/view', (req, res) => {
+app.get('/view', checkAuth, (req, res) => {
     console.log(req.cookies);
     console.log(req.sessionID);
-    console.log(req.session.users);
+    console.log(req.session.user);
     res.send(users);
 })
 
-// //logout
+//protected route
+app.get('/mycookie', checkAuth, (req, res) => {
+    res.send(`
+    <h1>This is protected route</h1>
+    <p>Since you're an authorized user</p>
+    <p>Here's some cookie</p>
+    <img src="https://image.shutterstock.com/image-vector/cute-flat-cartoon-cookies-illustration-600w-1738553351.jpg" alt="" width="400px">
+ `);
+})
 
-// app.get('/logout', (req, res) => {
-//     req.session.users.destroy()
-//     res.send(users);
-// })
+//logout
+
+app.get('/logout', (req, res) => {
+    req.session.destroy(() => {
+        console.log('logged out')
+    });
+    res.redirect('/');
+})
